c268d08a30e01022ddc324a1c294c9fcf799df61
Linda S <linda.steinberga.86@gmail.com>
2023-03-05 15:10:40 +0200

    Practical task:subtract five days

---
+++ name_age_check.py
@@ -0,0 +1,9 @@
+name = input('Please enter your name: ')
+age = int(input('Please enter your age: '))
+
+if age > 18:
+    print('You are welcome')
+elif age ==18:
+    print('Your are welcome too')
+else:
+    print('Your are to young to enter, pleas come when You are 18')
